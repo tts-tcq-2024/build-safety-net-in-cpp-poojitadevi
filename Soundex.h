45a3f8f91d321773602367708bd2cf86b380c858
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-03 17:33:44 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -2,8 +2,19 @@
 #define SOUNDEX_H
 
 #include <string>
+
 class Soundex {
-std::string generateSoundex(const std::string& name);
+public:
+    // Generates the Soundex code for a given name
+    std::string generate(const std::string& name) const;
+
+private:
+    // Returns the Soundex code for a single character
+    char getCode(char c) const;
+
+    // Pads the Soundex code to 4 digits with '0'
+    std::string padToFourDigits(const std::string& code) const;
 };
 
 #endif // SOUNDEX_H
+
