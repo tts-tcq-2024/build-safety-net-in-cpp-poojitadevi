7e085c40be5a27be595b3b80675a6a78e1861620
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-04 10:32:28 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -7,13 +7,13 @@ protected:
 };
 
 TEST_F(SoundexTest, HandlesEmptyString) {
-    EXPECT_EQ(soundex.generate(""), "");
+    EXPECT_EQ(soundex.generateSoundexCode(""), "");
 }
 
 TEST_F(SoundexTest, HandlesSingleCharacter) {
-    EXPECT_EQ(soundex.generate("A"), "A000");
+    EXPECT_EQ(soundex.generateSoundexCode("A"), "A000");
 }
 
 TEST_F(SoundexTest, HandlesLessThanFourCharacters) {
-    EXPECT_EQ(soundex.generate("Bounce"), "B520");
+    EXPECT_EQ(soundex.generateSoundexCode("Bounce"), "B520");
 }
