6068ddcb3825cc84424b5701861535089ea42e9c
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-09 18:01:41 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -3,62 +3,62 @@
 
 
 TEST(SoundexTest, HandlesEmptyString) {
-    EXPECT_EQ(generateSoundex(""), "");
+    EXPECT_EQ(generateCode(""), "");
 }
 
 TEST(SoundexTest, HandlesSymbols) {
-    EXPECT_EQ(generateSoundex("~bvv"), "");
+    EXPECT_EQ(generateCode("~bvv"), "");
 }
 
 TEST(SoundexTest, HandlesSingleCharater) {
-    EXPECT_EQ(generateSoundex("F"), "F000");
+    EXPECT_EQ(generateCode("F"), "F000");
 }
 
 TEST(SoundexTest, HandlesSingleLowerCharacter) {
-    EXPECT_EQ(generateSoundex("f"), "F000");
+    EXPECT_EQ(generateCode("f"), "F000");
 }
 
 TEST(SoundexTest, HandlesFirstTwoLettersSame) {
-    EXPECT_EQ(generateSoundex("BBert"), "B630");
+    EXPECT_EQ(generateCode("BBert"), "B630");
 }
 
 TEST(SoundexTest, HandlesFirstLetterAsNumber) {
-    EXPECT_EQ(generateSoundex("6ert"), "6630");
+    EXPECT_EQ(generateCode("6ert"), "6630");
 }
 
 TEST(SoundexTest, HandlesTwoVowels) {
-    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+    EXPECT_EQ(generateCode("Tymczak"), "T522");
 }
 
 TEST(SoundexTest, HandleStringWithVowels) {
-    EXPECT_EQ(generateSoundex("Aeiou"), "A000");
+    EXPECT_EQ(generateCode("Aeiou"), "A000");
 }
 
 TEST(SoundexTest, HandleStringWithOtherConsonants) {
-    EXPECT_EQ(generateSoundex("Chwy"), "C000");
+    EXPECT_EQ(generateCode("Chwy"), "C000");
 }
 
 TEST(SoundexTest, HandleFirstTwoLetters) {
-    EXPECT_EQ(generateSoundex("Pfister"), "P236");
+    EXPECT_EQ(generateCode("Pfister"), "P236");
 }
 
 TEST(SoundexTest, HandlesWithSpace) {
-    EXPECT_EQ(generateSoundex("6  ert"), "6630");
+    EXPECT_EQ(generateCode("6  ert"), "6630");
 }
 
 TEST(SoundexTest, HandleSpecialCharater) {
-    EXPECT_EQ(generateSoundex("B@ert"), "B630");
+    EXPECT_EQ(generateCode("B@ert"), "B630");
 }
 
 TEST(SoundexTest, HandleOnlyNumbers) {
-    EXPECT_EQ(generateSoundex("6630"), "6000");
+    EXPECT_EQ(generateCode("6630"), "6000");
 }
 
 TEST(SoundexTest, HandleNumberAndString) {
-    EXPECT_EQ(generateSoundex("6630ff"), "6100");
-     EXPECT_EQ(generateSoundex("6630fht"), "6130");
-    EXPECT_EQ(generateSoundex("6630aei"), "6000");
-    EXPECT_EQ(generateSoundex("6630hwy"), "6000");
-    EXPECT_EQ(generateSoundex("6630Aj#$f"), "6210");
+    EXPECT_EQ(generateCode("6630ff"), "6100");
+     EXPECT_EQ(generateCode("6630fht"), "6130");
+    EXPECT_EQ(generateCode("6630aei"), "6000");
+    EXPECT_EQ(generateCode("6630hwy"), "6000");
+    EXPECT_EQ(generateCode("6630Aj#$f"), "6210");
     
 }
