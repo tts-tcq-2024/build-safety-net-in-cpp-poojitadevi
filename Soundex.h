e468900699dffd4747e2e1425910878e14e9b724
poojitadevi <60169987+poojitadevi@users.noreply.github.com>
2024-09-11 19:59:24 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -3,19 +3,6 @@
 
 #include <string>
 
-class Soundex {
-public:
-    // Generates the Soundex code for a given name    
-  std::string generateCode(const std::string& name) const; // Method to generate code
-
-private:
-    // Returns the Soundex code for a single character
-    char getCode(char c) const;
-    std::string applyPadding(const std::string& soundex) const; // Method for padding
-    std::string initializeSoundex(const std::string& soundex) const;
-    void processRemainingCharacters(const std::string& name, std::string& soundex, char& prevCode) const;
-    void appendCodeIfValid(char code, char& prevCode, std::string& soundex) const;
-};
+std::string generateSoundex(const std::string& name);
 
 #endif // SOUNDEX_H
-
